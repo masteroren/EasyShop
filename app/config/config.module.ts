2e06650fc15882e37738003a5fcbbcf82aa4e01f
masteroren <master.oren@gmail.com>
2016-09-19 23:14:24 +0300

    Add config constructor to init values

--- app/config/config.module.ts
+++ app/config/config.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { ConfigComponent } from "./config.component";
+import { ConfigService } from "../shared/config.service";
 
 @NgModule({
   exports: [ConfigComponent],
   declarations: [
     ConfigComponent,
-  ]
+  ],
+  providers: [ConfigService]
 })
 export class ConfigModule {}

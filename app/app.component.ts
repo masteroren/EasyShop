664e138a089e3322d105a37fcb520a5a9f3ce3e4
masteroren <master.oren@gmail.com>
2016-09-22 07:50:37 +0300

    Add debug option, Change to home page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from "@angular/core";
-import { RouterExtensions } from "nativescript-angular/router/router-extensions";
-import observable = require("data/observable");
 
 @Component({
     selector: "easy-shope-main",
     templateUrl: "app.component.html",
 })
 export class AppComponent {
-
-    constructor(private routerExtensions: RouterExtensions){}
-
-    config(args: observable.EventData){
-        this.routerExtensions.navigate(['/config']);
-    }
 }
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -2,26 +2,43 @@ import { Component } from "@angular/core";
 import { ScanService } from "../shared/scan.service";
 import { RouterExtensions } from "nativescript-angular/router/router-extensions";
 
+import timer = require("timer");
+
+import { StoreService } from "../shared/store.service";
+import { StoreModel } from "../shared/models/store.model";
+
 @Component({
     selector: 'home',
     templateUrl: 'home/home.component.html',
-    providers:[ScanService]
+    styleUrls: ['home/home.css'],
+    providers:[ScanService, StoreService]
 })
 export class HomeComponent{
     storeBarcode: string;
     storeExist: boolean = false;
+    storeName: string;
+    store: StoreModel;
 
     constructor(private scanService: ScanService, 
-        private routerExtensions: RouterExtensions){}
+        private routerExtensions: RouterExtensions,
+        private storeService: StoreService){}
 
     scanStore(){
-        this.scanService.scan().then((result) => {
-            this.storeBarcode = result;
-            this.storeExist = true;
+        this.scanService.scan().then((barcode) => {
+            this.store = this.storeService.getStore(barcode)
+            if (this.store){
+                this.storeBarcode = this.store.barcode;
+                this.storeExist = true;
+                this.storeName = "Welcom to " + this.store.name;
+
+                timer.setTimeout(() => {
+                    this.routerExtensions.navigate(["/product"]);
+                }, 2000);
+            }
         });
     }
 
-    goToProduct(){
-        this.routerExtensions.navigate(["/product"]);
-    }
+    // goToProduct(){
+    //     this.routerExtensions.navigate(["/product"]);
+    // }
 }

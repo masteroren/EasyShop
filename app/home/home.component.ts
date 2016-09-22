96691cf3cb3243b9057944b82a2c46ef09f7d864
masteroren <master.oren@gmail.com>
2016-09-23 00:16:44 +0300

    Home fix

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -16,15 +16,12 @@ import { StoreModel } from "../shared/models/store.model";
 export class HomeComponent {
     store: StoreModel;
 
-    welcomText: string;
-
     constructor(private scanService: ScanService,
         private routerExtensions: RouterExtensions,
         private storeService: StoreService) { }
 
     ngOnInit() {
         console.log("Home -> Init");
-        this.welcomText = "סרוק את הברקוד בכניסה לסופר ובוא נתחיל לתקתק את הקניות!";
     }
 
     scanStore() {

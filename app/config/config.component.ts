83744b82d8c061ff6dd9aeb829cbd8874874c142
masteroren <master.oren@gmail.com>
2016-09-23 10:01:57 +0300

    Config

--- app/config/config.component.ts
+++ app/config/config.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import { ConfigService } from "./config.service";
 
 @Component({
     selector: 'config',
-    templateUrl: 'config/config.component.html'
+    templateUrl: 'config/config.component.html',
+    styleUrls: ['config/config.css']
 })
-export class ConfigComponent{
+export class ConfigComponent {
+
+    constructor(public configService: ConfigService) {
+        console.log('ConfigComponent constructor -> ' + this.configService.model.showBonuses);
+    }
+
+    bonusTap() {
+        if (this.configService.model.showBonuses === false)
+            this.configService.model.showBonuses = true;
+        else
+            this.configService.model.showBonuses = false;
+        console.log('Bonuses tapped -> ' + this.configService.model.showBonuses);
+    }
 }
+
---
+++ app/config/config.model.ts
@@ -0,0 +1,10 @@
+export class ConfigModel {
+    showBonuses: boolean;
+    showNutrition: boolean;
+
+    constructor(showBonuses: boolean, showNutrition: boolean){
+        this.showBonuses = showBonuses;
+        this.showNutrition = showNutrition;
+    }
+}
+    

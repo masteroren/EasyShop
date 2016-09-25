b72654deab46f642a6048e25b11c713bd3cc068a
masteroren <master.oren@gmail.com>
2016-09-25 22:48:33 +0300

    Add platform identify

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
+import application = require("application");
 
 @Component({
     selector: "easy-shope-main",
     templateUrl: "app.component.html",
+    styleUrls: ["app.css"]
 })
 export class AppComponent {
+
+    constructor(){}
+
+    ngOnInit(){
+        if (application.android){
+
+        }
+
+        if (application.ios){
+            
+        }
+    }
 }
